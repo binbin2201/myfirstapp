10dff216455b17ba5c5ceadacbce0bf32806f15e
Zhao <ninjatuna970@gmail.com>
2022-05-24 14:21:22 +0800

    Initial commit

---
+++ src/main/java/com/bin/application/App.java
@@ -0,0 +1,11 @@
+package com.bin.application;
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setSize(1600,900);
+        window.setVisible(true);
+    }
+}
